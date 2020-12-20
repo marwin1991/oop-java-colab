8ceb1da3578888846f7028b1580b0244779bbb6c
Dawid <64140934+BD4vid777@users.noreply.github.com>
2020-12-20 14:10:47 +0100

    starting

--- unit2/waste-recycling-java/src/main/java/com/codecool/wasterecycling/Dustbin.java
+++ unit2/waste-recycling-java/src/main/java/com/codecool/wasterecycling/Dustbin.java
@@ -1,4 +1,26 @@
 package com.codecool.wasterecycling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dustbin {
+
+        private String color;
+        private List<Garbage> bin;
+
+        public Dustbin(String color) {
+                this.color = color;
+                this.bin = new ArrayList<>();
+        }
+
+        public String getColor() {
+                return color;
+        }
+
+        public void throwOutGarbage(Garbage garbage) {
+                if (garbage instanceof PaperGarbage) {
+                        bin.add(garbage);
+                }
+
+        }
 }
--- unit2/waste-recycling-java/src/main/java/com/codecool/wasterecycling/Main.java
+++ unit2/waste-recycling-java/src/main/java/com/codecool/wasterecycling/Main.java
@@ -3,5 +3,6 @@ package com.codecool.wasterecycling;
 public class Main {
 
     public static void main(String[] args) {
+            System.out.println("Waste Project");
     }
 }
